49eb9810216593cb4554d10649219a3f87d2d459
Jason Cleveland <jason.cleveland@unb.ca>
2019-09-22 14:39:35 -0300

    Add code for Lab 5 Part 2
    Update odds-evens.rkt to include odds and evens calculator function
    rewritten to use for loops.
    Add function in odds-evens.rkt to test the three function iterations
    with large numbers.
    Add screenshot of large number test results.

--- labs/L05/odds-evens.rkt
+++ labs/L05/odds-evens.rkt
@@ -17,8 +17,24 @@
       [(even? (first lst)) (helper (rest lst) odds (add1 evens))]))
   (helper lst 0 0))
 
+(define (odds-evens3 lst)
+  (for/fold
+   ([accumulator (list 0 0)])
+   ([n lst])
+    (cond
+      [(odd? n) (map + '(1 0) accumulator)]
+      [(even? n) (map + '(0 1) accumulator)])))
+
+(define big-list (range 1000000))
+(for* ([fun (list odds-evens odds-evens2 odds-evens3)]
+       [rep '(1 2 3)])
+  (printf "~a ~a\n" fun rep)
+  (time (fun big-list)))
+
 (module+ test
   (check-equal? (odds-evens (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4))
   (define random-list (build-list 100 (lambda (x) (random 1 100))))
   (check-equal? (odds-evens2 (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4))
-  (check-equal? (odds-evens random-list) (odds-evens2 random-list)))
+  (check-equal? (odds-evens random-list) (odds-evens2 random-list))
+  (check-equal? (odds-evens3 (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4))
+  (check-equal? (odds-evens random-list) (odds-evens3 random-list)))
