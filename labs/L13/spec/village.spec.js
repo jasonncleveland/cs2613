5fd807cd0deafba1d797aeee81d293e3ac8f71b7
Jason Cleveland <jason.cleveland@unb.ca>
2019-10-17 12:07:48 -0300

    Add Code from Lab 12 to Lab 13

---
+++ labs/L13/spec/village.spec.js
@@ -0,0 +1,78 @@
+let village = require("../village.js");
+let VillageState = village.VillageState;
+
+describe("roadGraph",
+    function () {
+        let roadGraph = village.roadGraph;
+
+        it("Alice's House",
+            function () {
+                expect(roadGraph["Alice's House"]).toEqual(["Bob's House", "Cabin", "Post Office"]);
+            });
+
+        it("Bob's House",
+            function () {
+                expect(roadGraph["Bob's House"]).toEqual(jasmine.objectContaining(["Alice's House"]));
+            });
+    });
+
+
+describe("VillageState",
+    function () {
+        let first = new VillageState(
+            "Post Office",
+            [{ place: "Post Office", address: "Alice's House" }, { place: "Mars", destination: "Jupiter" }]
+        );
+
+        let next = first.move("Alice's House");
+
+        it("move changes place",
+            function () {
+                expect(next.place).toEqual("Alice's House");
+            });
+
+        it("move changes parcels",
+            function () {
+                expect(next.parcels).toEqual([{ place: "Mars", destination: "Jupiter" }]);
+            });
+
+        it("immutability",
+            function () {
+                expect(first.place).toEqual("Post Office");
+            });
+
+        it("invalid destination",
+            function () {
+                expect(first.move("Mars")).toBe(first);
+            });
+
+    });
+
+
+describe("runRobot",
+    function () {
+        it("no parcels",
+            function () {
+                console.log = jasmine.createSpy("log");
+                let noParcels = new VillageState("Post Office", []);
+                village.runRobot(noParcels);
+
+                expect(console.log).toHaveBeenCalledWith("Done in 0 turns");
+            });
+
+        it("random robot",
+            function () {
+                console.log = jasmine.createSpy("log");
+                village.runRobot(VillageState.random(), village.randomRobot);
+
+                expect(console.log).toHaveBeenCalledWith(jasmine.stringMatching(/^Done/));
+            });
+
+        it("route robot",
+            function () {
+                console.log = jasmine.createSpy("log");
+                village.runRobot(VillageState.random(), village.routeRobot, []);
+
+                expect(console.log).toHaveBeenCalledWith(jasmine.stringMatching(/^Done/));
+            });
+    });
