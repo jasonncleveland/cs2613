0161124dfb492f2a939f9dbb87e98a7279faeda7
Jason Cleveland <jason.cleveland@unb.ca>
2019-11-05 11:52:00 -0400

    Add Code for Lab 17 Part 3
    Update split_csv function to remove quotes from parsed data using a new
    strip quotes function. The strip quotes function uses regular
    expressions.

--- labs/L17/parse_csv.py
+++ labs/L17/parse_csv.py
@@ -1,6 +1,17 @@
+import re
+
+def strip_quotes(string):
+    strip_regex = re.compile(r'^"?([^"]+)"?$')
+    search = strip_regex.search(string)
+    if search:
+        return search.group(1)
+    else:
+        return None
+
 def split_csv(string):
     output = []
     lines = string.splitlines()
     for line in lines:
-        output.append(line.split(','))
+        strip_quotes(line)
+        output.append([strip_quotes(element) for element in line.split(',')])
     return output
--- labs/L17/test_parse_csv.py
+++ labs/L17/test_parse_csv.py
@@ -1,4 +1,4 @@
-from parse_csv import split_csv
+from parse_csv import split_csv, strip_quotes
 
 test_string_1 = """OPEID,INSTNM,TUITIONFEE_OUT
 02503400,Amridge University,6900
@@ -21,5 +21,22 @@ table1 = [
     ['00101800', 'George C Wallace State Community College-Dothan', '7710']
 ]
 
+test_string_2 = '''OPEID,INSTNM,TUITIONFEE_OUT
+02503400,"Amridge University",6900
+00100700,"Central Alabama Community College",7770
+01218200,"Chattahoochee Valley Community College",7830
+00101500,"Enterprise State Community College",7770
+00106000,"James H Faulkner State Community College",7770
+00101700,"Gadsden State Community College",5976
+00101800,"George C Wallace State Community College-Dothan",7710
+'''
+
 def test_split_1():
     assert split_csv(test_string_1) == table1
+
+def test_strip_quotes():
+    assert strip_quotes('"hello"') == 'hello'
+    assert strip_quotes('hello') == 'hello'
+
+def test_split_2():
+    assert split_csv(test_string_2) == table1
