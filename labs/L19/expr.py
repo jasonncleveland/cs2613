3fb088676e2dc78ea35829256ea5954b6bf4d9d3
Jason Cleveland <jason.cleveland@unb.ca>
2019-11-19 13:55:27 -0400

    Add Code for Lab 19 Part 3
    Update expr.py to include eval function. The eval function recursively
    evaluates the left and right operands to handle nested expressions.

--- labs/L19/expr.py
+++ labs/L19/expr.py
@@ -10,3 +10,12 @@ class Expr:
             and self.left == other.left
             and self.right == other.right
         )
+
+    def eval(self):
+        left = self.left.eval() if isinstance(self.left, Expr) else self.left
+        right = self.right.eval() if isinstance(self.right, Expr) else self.right
+
+        if self.operator == '+':
+            return left + right
+        elif self.operator == '*':
+            return left * right
--- labs/L19/test_expr.py
+++ labs/L19/test_expr.py
@@ -12,3 +12,17 @@ def test_equality():
     assert compound1 == deepcopy(compound1)
     assert compound2 == deepcopy(compound2)
     assert compound3 == deepcopy(compound3)
+
+def test_basic():
+    assert six_plus_nine.eval() == 15
+    assert six_times_nine.eval() == 54
+    assert deepcopy(six_plus_nine).eval() == 15
+    assert deepcopy(six_times_nine).eval() == 54
+
+def test_recur():
+    assert compound1.eval() == 69
+    assert compound2.eval() == 3726
+    assert compound3.eval() == 3729
+    assert deepcopy(compound1).eval() == 69
+    assert deepcopy(compound2).eval() == 3726
+    assert deepcopy(compound3).eval() == 3729
