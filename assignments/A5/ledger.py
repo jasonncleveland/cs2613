393110017b6701f9e6463c610806af232fae828a
Jason Cleveland <jason.cleveland@unb.ca>
2019-11-22 03:30:50 -0400

    Remove global variable

--- assignments/A5/ledger.py
+++ assignments/A5/ledger.py
@@ -2,8 +2,6 @@ from scanner import Scanner
 from type_ import Type
 
 
-ACCOUNTS = {}
-
 ACTION_OPEN = [Type.OPEN, Type.IDENT, Type.CURRENCY]
 ACTION_TRANSFER = [Type.TRANSFER, Type.IDENT, Type.IDENT, Type.CURRENCY]
 ACTION_BALANCE = [Type.BALANCE, Type.IDENT]
@@ -16,31 +14,32 @@ def add_balances(l_balance: int, r_balance: int) -> int:
     return float(''.join("{0:.2f}".format(left + right).split('.')))
 
 
-def open_account(account_name: str, starting_balance: int):
+def open_account(accounts: dict, account_name: str, starting_balance: int):
     """opens a new account with the specified starting balance"""
-    ACCOUNTS[account_name] = starting_balance
+    accounts[account_name] = starting_balance
 
 
-def transfer_balance(from_account: str, to_account: str, balance: int):
+def transfer_balance(accounts: dict, from_account: str, to_account: str, balance: int):
     """removes desired funds from first account and adds them to second"""
-    ACCOUNTS[from_account] = add_balances(ACCOUNTS[from_account], balance * -1)
-    ACCOUNTS[to_account] = add_balances(ACCOUNTS[to_account], balance)
+    accounts[from_account] = add_balances(accounts[from_account], balance * -1)
+    accounts[to_account] = add_balances(accounts[to_account], balance)
 
 
-def account_balance(account_name: str) -> tuple:
+def account_balance(accounts: dict, account_name: str) -> tuple:
     """returns a tuple of the account name and current balance"""
-    return (account_name, ACCOUNTS.get(account_name, 0))
+    return (account_name, accounts.get(account_name, 0))
 
 
 def ledger(input: str):
     """manages ledger actions passed in via a newline separated string"""
+    accounts = {}
     actions = [list(Scanner(line)) for line in input.strip().splitlines()]
 
     for action in actions:
         action_type = [token.type for token in action]
         if action_type == ACTION_OPEN:
-            open_account(action[1].value, action[2].value)
+            open_account(accounts, action[1].value, action[2].value)
         elif action_type == ACTION_TRANSFER:
-            transfer_balance(action[1].value, action[2].value, action[3].value)
+            transfer_balance(accounts, action[1].value, action[2].value, action[3].value)
         elif action_type == ACTION_BALANCE:
-            yield account_balance(action[1].value)
+            yield account_balance(accounts, action[1].value)
